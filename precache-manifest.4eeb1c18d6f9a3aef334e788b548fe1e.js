442dd2904c692da063a92e9c7c931877d614fa49
lilpolymath <ayofavour7@gmail.com>
2020-05-06 13:41:21 +0100

    Updates

---
+++ precache-manifest.4eeb1c18d6f9a3aef334e788b548fe1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7634c18d01d3960e19c7ff51843818",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "d16bb2c6d1b6f85efd17",
+    "url": "/covid19/static/css/main.4d5a3da0.chunk.css"
+  },
+  {
+    "revision": "3302288e21d91a49fe68",
+    "url": "/covid19/static/js/2.24bf2c66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.24bf2c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16bb2c6d1b6f85efd17",
+    "url": "/covid19/static/js/main.70af2530.chunk.js"
+  },
+  {
+    "revision": "1ab524a144c31c7907ff",
+    "url": "/covid19/static/js/runtime-main.6df57145.js"
+  }
+]);
