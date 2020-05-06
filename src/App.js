8a6ab4a9b0a58df52fcb699ea4acb8ec5e2eae77
lilpolymath <ayofavour7@gmail.com>
2020-05-06 11:30:16 +0100

    feat: Symptoms component and mobile responsiveness.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar/NavBar';
 import Hero from './components/Hero/Hero';
 import Overview from './components/Overview/Overview';
 import Contagion from './components/Contagion/Contagion';
+import Symptoms from './components/Symptoms/Symptoms';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Overview />
       <Contagion />
+      <Symptoms />
     </>
   );
 }
